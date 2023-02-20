c3c207ef0c190cc663f226061ae95aa5d95ff424
mark.molenmaker <mark.molenmaker@technolution.nl>
2023-02-20 11:18:43 +0100

    Updated .env

--- src/main.js
+++ src/main.js
@@ -5,6 +5,6 @@ import router from './router'
 import store from './store'
 import axios from "axios";
 
-axios.defaults.baseURL = 'https://ojo3a4gtxi.execute-api.eu-central-1.amazonaws.com'
+axios.defaults.baseURL = process.env.VUE_APP_API_ENDPOINT
 
 createApp(App).use(store).use(router).mount('#app')
